2fd76a7da27ef24c0ca11f5475eb5664180eefbf
Kanan Gurbanov <knanqurbanov22@gmail.com>
2025-02-21 16:36:01 +0400

    added Dockerfile

--- app/webdriver/webdriver.py
+++ app/webdriver/webdriver.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(options=chrome_options)
+chrome_options.binary_location = "/usr/bin/chromium"
+
+chrome_driver_service = Service(executable_path="/usr/bin/chromedriver")
+
+driver = webdriver.Chrome(service=chrome_driver_service, options=chrome_options)
