80a57bce59c7f47e3ddb2c2ca3d0d070ec06494d
Kanan Gurbanov <knanqurbanov22@gmail.com>
2025-06-07 23:16:55 +0400

    readme added

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,6 @@
-FROM python:3.10-slim
+FROM python:3.11-slim
 
 WORKDIR /app
-
-RUN apt-get update && apt-get install -y \
-    wget \
-    unzip \
-    curl \
-    chromium \
-    chromium-driver
-
-ENV CHROME_BIN=/usr/bin/chromium
-ENV CHROMEDRIVER_BIN=/usr/bin/chromedriver
 
 COPY . /app
 
--- app/webdriver/webdriver.py
+++ app/webdriver/webdriver.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--no-sandbox")
