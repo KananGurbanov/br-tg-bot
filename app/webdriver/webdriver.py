42136951d4c3855731fb60afb5e2f742a6a98c82
Kanan Gurbanov <knanqurbanov22@gmail.com>
2025-06-07 22:18:16 +0400

    some changes

--- app/webdriver/webdriver.py
+++ app/webdriver/webdriver.py
@@ -3,13 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--no-sandbox")
 
-chrome_options.binary_location = "/usr/bin/chromium"
-
-chrome_driver_service = Service(executable_path="/usr/bin/chromedriver")
-
-driver = webdriver.Chrome(service=chrome_driver_service, options=chrome_options)
+# Use built-in Selenium Manager to automatically fetch and use the correct driver
+driver = webdriver.Chrome(options=chrome_options)
